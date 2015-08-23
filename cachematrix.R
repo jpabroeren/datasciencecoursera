48c9fc0cc19b8be63540ea137ca251f920af7e93
jpabroeren <jpabroeren@Macbook.local>
2015-08-24 00:32:35 +0200

    Added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,19 +2,28 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function accepts a Matrix as parameter, and creates with it, a new matrix-like list.
+## This list consists of the original matrix, a couple of functions, and an inverse matrix
+## These functions make it possible to check whether the original matrix hasn't changed. 
+## If not, and the inverse matrix was already created, the 'cached' inverse matrix is returned
+## If the original matrix has changed, or the inverse matrix wasn't created yet, the inverse matrix is generated.
 
 makeCacheMatrix <- function(myMatrix = matrix()) {
+    ##Reset:
     globalMatrix <- NULL
+    ##Define the set-function
     set <- function(newValue) {
+        ##Create a new matrix in our 'parent'
         myMatrix <<- newValue
         ##Because the matrix has changed, any cached data is obsolete know
         globalMatrix <<- NULL
     }
+    ##Define the get-function (only returns the desired matrix)
     get <- function() myMatrix
+    ##setInversematrix organises the 'storing' of the invertedmatrix
     setInversematrix <- function(invertedMatrix) globalMatrix <<- invertedMatrix
+    ##the getInverseMatrix simply returns the inversedMatrix
     getInverseMatrix <- function() globalMatrix
-    setmean <- function(mean) m <<- mean
-    getmean <- function() globalMatrix
     list(set = set, get = get,
          setInversematrix = setInversematrix,
          getInverseMatrix = getInverseMatrix)
@@ -31,7 +40,7 @@ cacheSolve <- function(myMatrix, ...) {
         ##globalMatrix exists. But are we sure it is the same?
         ##That means the inversematrix is up-to-date. myMatrix$set NULLs the globalmatrix,
         ##so an update of the matrix will result in a clearance of the cache
-        ##Great, so we kan use the cached data
+        ##Great, so we can use the cached data
         message("getting cached data")
         return(globalMatrix)   
     } else {
